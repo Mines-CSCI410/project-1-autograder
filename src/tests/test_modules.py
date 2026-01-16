8d0130c2aeb9baf7b54dd56685f60a48c833a67f
Grant Lemons <grantlemons@aol.com>
2026-01-16 13:17:23 -0700

    get rid of xor16

--- src/tests/test_modules.py
+++ src/tests/test_modules.py
@@ -27,82 +27,77 @@ class TestBase(unittest.TestCase):
             raise AssertionError('Module output does not mach the expected output!')
 
 class TestModules(TestBase): 
-    @weight(95/16)
+    @weight(95/15)
     @number(1)
     def test_and(self):
         self.assertModulePasses('and')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(2)
     def test_or(self):
         self.assertModulePasses('or')
 
-    @weight(95/16)
-    @number(3)
-    def test_xor16(self):
-        self.assertModulePasses('xor')
-
-    @weight(95/16)
+    @weight(95/15)
     @number(4)
     def test_not(self):
         self.assertModulePasses('not')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(5)
     def test_mux(self):
         self.assertModulePasses('mux')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(6)
     def test_dmux(self):
         self.assertModulePasses('dmux')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(7)
     def test_and16(self):
         self.assertModulePasses('and16')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(8)
     def test_or16(self):
         self.assertModulePasses('or16')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(9)
     def test_not16(self):
         self.assertModulePasses('not16')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(10)
     def test_xor(self):
         self.assertModulePasses('xor16')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(11)
     def test_mux16(self):
         self.assertModulePasses('mux16')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(12)
     def test_mux4way16(self):
         self.assertModulePasses('mux4way16')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(13)
     def test_mux8way16(self):
         self.assertModulePasses('mux8way16')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(14)
     def test_dmux4way(self):
         self.assertModulePasses('dmux4way')
 
-    @weight(95/16)
+    @weight(95/15)
     @number(15)
     def test_dmux8way(self):
         self.assertModulePasses('dmux8way')
 
-    @weight(95/16)
-    @number(16)
+    @weight(95/15)
+    @number(15)
     def test_or8way(self):
         self.assertModulePasses('or8way')
